db1e1f5036ff84f0a67e741fe4edf8ad04779e72
Pandeng Li <pdli@mobvoi.com>
2016-04-20 19:02:40 +0800

    add nodeapi file into project

---
+++ mobile/src/main/java/com/mobvoi/ticwear/apisimpledemo/NodeApiFragment.java
@@ -0,0 +1,20 @@
+package com.mobvoi.ticwear.apisimpledemo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by pd on 16-4-20.
+ */
+public class NodeApiFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                                    Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.)
+    }
+}
